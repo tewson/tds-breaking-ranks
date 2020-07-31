72e853666ac3d1237f03d75de31466f37f3399a5
Tewson Seeoun <tewson.seeoun@gmail.com>
2020-07-31 11:41:12 +0100

    Remove test rendering of members

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import type { Member, Vote } from "./api";
-  import { fetchMembers, fetchVotes } from "./api";
+  import type { Vote } from "./api";
+  import { fetchVotes } from "./api";
 
-  let members: Member[] = [];
   let votes: Vote[] = [];
 
   async function init() {
-    members = await fetchMembers();
     votes = await fetchVotes();
   }
 
@@ -15,11 +13,6 @@
 
 <main>
   <h1>Breaking Ranks</h1>
-  <ul>
-    {#each members as member}
-      <li>{member.fullName}</li>
-    {/each}
-  </ul>
   {#each votes as vote}
     <div>
       <h1>{vote.debateTitle}</h1>
